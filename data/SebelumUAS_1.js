65687a33531f0fe5f89dc493668cee1c55155d04
Aprizza <56188101+Aprizza@users.noreply.github.com>
2020-12-15 21:46:00 +0700

    Add files via upload

---
+++ data/SebelumUAS_1.js
@@ -0,0 +1,8 @@
+var json_SebelumUAS_1 = {
+"type": "FeatureCollection",
+"name": "SebelumUAS_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ -3.189802599666851, 55.953269773530153 ] } }
+]
+}
